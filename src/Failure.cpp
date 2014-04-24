85dd6965d8d141d8769a18e8e65da9e1a5b1a7e0
Sam Mikes <smikes@cubane.com>
2014-04-24 21:22:44 +0100

    failure: add const qualifier to const string

--- src/Failure.cpp
+++ src/Failure.cpp
@@ -3,48 +3,46 @@
 #include "Failure.h"
 
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 
 
-Failure::Failure (const SimpleString&	theTestName, 
-				  const SimpleString&	theFileName, 
+Failure::Failure (const SimpleString&	theTestName,
+				  const SimpleString&	theFileName,
 		          long	 				theLineNumber,
-		          const SimpleString&	theCondition) 
-: message (theCondition), 
-  testName (theTestName), 
-  fileName (theFileName), 
+		          const SimpleString&	theCondition)
+: message (theCondition),
+  testName (theTestName),
+  fileName (theFileName),
   lineNumber (theLineNumber)
 {
 }
 
 
-Failure::Failure (const SimpleString&	theTestName, 
-			 	  const SimpleString&	theFileName, 
+Failure::Failure (const SimpleString&	theTestName,
+			 	  const SimpleString&	theFileName,
 				  long					theLineNumber,
 				  const SimpleString&	expected,
-				  const SimpleString&	actual) 
-: testName (theTestName), 
-  fileName (theFileName), 
+				  const SimpleString&	actual)
+: testName (theTestName),
+  fileName (theFileName),
   lineNumber (theLineNumber)
 {
-	char *part1 = "expected ";
-	char *part3 = " but was: ";
+	const char *part1 = "expected ";
+	const char *part3 = " but was: ";
 
-	char *stage = new char [strlen (part1) 
-					+ expected.size () 
+	char *stage = new char [strlen (part1)
+					+ expected.size ()
 					+ strlen (part3)
 					+ actual.size ()
 					+ 1];
 
-	sprintf(stage, "%s%s%s%s", 
-		part1, 
-		expected.asCharString(), 
-		part3, 
+	sprintf(stage, "%s%s%s%s",
+		part1,
+		expected.asCharString(),
+		part3,
 		actual.asCharString());
 
 	message = SimpleString(stage);
 
 	delete stage;
 }
-
-
